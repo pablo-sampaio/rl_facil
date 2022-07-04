93d5f62d1023ac1e70a28a86b797a13cb4f0175a
Pablo Sampaio <prof.pablo.sampaio@gmail.com>
2022-07-04 14:38:45 -0300

    Minor change

--- 2_ma_bandits/experiments.py
+++ 2_ma_bandits/experiments.py
@@ -19,7 +19,7 @@ results.append( repeated_exec(EXECUTIONS, "GREEDY", run_greedy, mab_problem) )
 results.append( repeated_exec(EXECUTIONS, "RANDOM", run_random, mab_problem) )
 results.append( repeated_exec(EXECUTIONS, "UCB", run_ucb, mab_problem) )
 epsilon = 0.1
-results.append( repeated_exec(EXECUTIONS, "EPS({epsilon})-GREEDY", run_epsilon_greedy, mab_problem, epsilon) )
+results.append( repeated_exec(EXECUTIONS, f"EPS({epsilon})-GREEDY", run_epsilon_greedy, mab_problem, epsilon) )
 
 
 plot_results(results, mab_problem.get_max_mean_reward())
