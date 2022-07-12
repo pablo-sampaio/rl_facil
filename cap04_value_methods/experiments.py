dcf9ef745a37acc950e6ab7e7e39adad91693e22
Pablo Sampaio <prof.pablo.sampaio@gmail.com>
2022-07-12 12:50:35 -0300

    Mais explicações e correções de bug após a aula

--- cap04_value_methods/experiments.py
+++ cap04_value_methods/experiments.py
@@ -1,7 +1,8 @@
 import gym
 
 from util import repeated_exec, plot_results
-from montecarlo_v0 import run_montecarlo
+from montecarlo_v1 import run_montecarlo1
+from montecarlo_v2 import run_montecarlo2
 
 EXECUTIONS = 1
 
@@ -9,7 +10,9 @@ enviroment = gym.make("Taxi-v3")
 
 results = []
 
-results.append( repeated_exec(1, "Monte-Carlo", run_montecarlo, enviroment, 1000) )
+results.append( repeated_exec(1, "Monte-Carlo1", run_montecarlo1, enviroment, 20000) )
 
+for lr in [0.01, 0.05, 0.1]:
+    results.append( repeated_exec(1, f"Monte-Carlo2(LR={lr})", run_montecarlo2, enviroment, 20000, lr) )
 
 plot_results(results, cumulative=False)
--- cap04_value_methods/montecarlo_v2.py
+++ cap04_value_methods/montecarlo_v2.py
@@ -15,7 +15,7 @@ from wrappers import save_rewards_plot, test_greedy_Q_policy
 
 
 # Esta é a política. Neste caso, escolhe uma ação com base nos valores
-# da tablea Q, usando uma estratégia epsilon-greedy.
+# da tabela Q, usando uma estratégia epsilon-greedy.
 def choose_action(Q, state, num_actions, epsilon):
     if np.random.random() < epsilon:
         return np.random.randint(0, num_actions)
@@ -23,15 +23,14 @@ def choose_action(Q, state, num_actions, epsilon):
         return np.argmax(Q[state])
 
 
-# Algoritmo Monte-Carlo de Controle, variante "Toda-visita".
-# Atenção: o espaços de estados e de ações precisam ser discretos, dados por valores inteiros
-def run_montecarlo(env, episodes, gamma=0.95, epsilon=0.01, render=False):
-    # assert?
+# Algoritmo Monte-Carlo de Controle, variante "toda-visita".
+# Atenção: os espaços de estados e de ações precisam ser discretos, dados por valores inteiros
+def run_montecarlo2(env, episodes, lr=0.1, gamma=0.95, epsilon=0.1, render=False):
+    assert isinstance(env.observation_space, gym.spaces.Discrete)
+    assert isinstance(env.action_space, gym.spaces.Discrete)
+    
     num_actions = env.action_space.n
     
-    # dicionário com todos os retornos descontados, para cada par (estado,ação)
-    returns_history = dict()
-
     # inicializa a tabela Q toda com zero,
     # usar o estado como índice das linhas e a ação como índice das colunas
     Q = np.zeros(shape = (env.observation_space.n, num_actions))
@@ -50,8 +49,8 @@ def run_montecarlo(env, episodes, gamma=0.95, epsilon=0.01, render=False):
         # PARTE 1: executa um episódio completo
         while done != True:   
             # exibe/renderiza os passos no ambiente, durante 1 episódio a cada mil e também nos últimos 5 episódios 
-            #if render and (i >= (episodes - 5) or (i+1) % 1000 == 0):
-            #    env.render()
+            if render and (i >= (episodes - 5) or (i+1) % 1000 == 0):
+                env.render()
                 
             # escolhe a próxima ação -- usa epsilon-greedy
             action = choose_action(Q, state, num_actions, epsilon)
@@ -67,23 +66,17 @@ def run_montecarlo(env, episodes, gamma=0.95, epsilon=0.01, render=False):
         
         sum_rewards_per_ep.append(sum_rewards)
 
-        # a cada 100 passos, imprime informação sobre o progresso 
+        # a cada 100 episódios, imprime informação sobre o progresso 
         if (i+1) % 100 == 0:
             avg_reward = np.mean(sum_rewards_per_ep[-100:])
             print(f"Episode {i+1} Average Reward (last 100): {avg_reward:.3f}")
 
         # PARTE 2: atualiza Q (e a política, implicitamente)
-        G = 0
+        Gt = 0
         for (s, a, r) in reversed(ep_trajectory):
-            G = r + gamma*G
-            
-            if returns_history.get((s,a)) is not None:
-                returns_history[s,a].append(G)
-            else:
-                returns_history[s,a] = [ G ]                    
-            
-            # média entre todas as ocorrências de (s,a) encontradas nos episódios
-            Q[s,a] = np.mean(returns_history[s,a]) # LENTO! -> vamos melhorar
+            Gt = r + gamma*Gt
+            delta = Gt - Q[s,a]
+            Q[s,a] = Q[s,a] + lr * delta
 
     return sum_rewards_per_ep, Q
 
@@ -93,19 +86,20 @@ if __name__ == "__main__":
     ENV_NAME = "Taxi-v3"
     r_max_plot = 10
 
-    EPISODES = 3000
+    EPISODES = 30000
+    LR = 0.01
     GAMMA = 0.95
     EPSILON = 0.1
 
     env = gym.make(ENV_NAME)
     
-    # Run Monte-Carlo RL control algorithm
-    Qtable, rewards = run_montecarlo(env, EPISODES, GAMMA, EPSILON, render=False)
-
-    # Save a file containing a graph episodes x rewards
-    filename = f"results/montecarlo-{ENV_NAME.lower()[0:8]}-ep{EPISODES}.png"
-    save_rewards_plot(rewards, r_max_plot, filename)
+    # Roda o algoritmo Monte-Carlo para o problema de controle (ou seja, para achar a política ótima)
+    rewards, Qtable = run_montecarlo2(env, EPISODES, LR, GAMMA, EPSILON, render=False)
     print("Últimos resultados: media =", np.mean(rewards[-20:]), ", desvio padrao =", np.std(rewards[-20:]))
+
+    # Salva um arquivo com o gráfico de passos x retornos (não descontados)
+    filename = f"results/montecarlo2-{ENV_NAME.lower()[0:8]}-ep{EPISODES}-lr{LR}.png"
+    save_rewards_plot(rewards, r_max_plot, filename)
 
     test_greedy_Q_policy(env, Qtable, 10, True)
     env.close()
