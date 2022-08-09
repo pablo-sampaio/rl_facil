13f78bab3c61beafe10ba8c7dcb6c06647d69b54
pablo-sampaio <pablo.sampaio@ufrpe.br>
2022-08-09 08:54:53 -0300

    Adicionando um agente Pong treinado

--- cap07/dqn_play.py
+++ cap07/dqn_play.py
@@ -1,58 +1,60 @@
 #!/usr/bin/env python3
 import gym
 import time
-import argparse
 import numpy as np
 
 import torch
 
-from lib import wrappers
-from lib import dqn_model
+import dqn_models
+import atari_wrappers
 
 import collections
 
-DEFAULT_ENV_NAME = "PongNoFrameskip-v4"
-FPS = 25
+
+ENV_NAME = "PongNoFrameskip-v4"
+ATARI_ENV = True
+MODEL_FILE = "cap07/PongNoFrameskip-v4-agente-treinado.net"
+
+VIDEO_DIR = None 
+RENDER = True
+
+DESIRED_FPS = 25
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--model", required=True, help="Model file to load")
-    parser.add_argument("-e", "--env", default=DEFAULT_ENV_NAME,
-                        help="Environment name to use, default=" + DEFAULT_ENV_NAME)
-    parser.add_argument("-r", "--record", help="Directory to store video recording")
-    parser.add_argument("--no-visualize", default=True, action='store_false', dest='visualize',
-                        help="Disable visualization of the game play")
-    args = parser.parse_args()
+if ATARI_ENV:
+    env = atari_wrappers.make_env_with_wrappers(ENV_NAME)
+else:
+    env = gym.make(ENV_NAME)
 
-    env = wrappers.make_env(args.env)
-    if args.record:
-        env = gym.wrappers.Monitor(env, args.record)
-    net = dqn_model.DQN(env.observation_space.shape, env.action_space.n)
-    net.load_state_dict(torch.load(args.model, map_location=lambda storage, loc: storage))
+if VIDEO_DIR is not None:
+    env = gym.wrappers.Monitor(env, VIDEO_DIR)
 
-    state = env.reset()
-    total_reward = 0.0
-    c = collections.Counter()
+net = dqn_models.DQNNet(env.observation_space.shape, env.action_space.n)
+net.load_state_dict(torch.load(MODEL_FILE, map_location=lambda storage, loc: storage))
 
-    while True:
-        start_ts = time.time()
-        if args.visualize:
-            env.render()
-        state_v = torch.tensor(np.array([state], copy=False))
-        q_vals = net(state_v).data.numpy()[0]
-        action = np.argmax(q_vals)
-        c[action] += 1
-        state, reward, done, _ = env.step(action)
-        total_reward += reward
-        if done:
-            break
-        if args.visualize:
-            delta = 1/FPS - (time.time() - start_ts)
-            if delta > 0:
-                time.sleep(delta)
-    print("Total reward: %.2f" % total_reward)
-    print("Action counts:", c)
-    if args.record:
-        env.env.close()
+state = env.reset()
+if RENDER:
+    env.render()
 
+total_reward = 0.0
+c = collections.Counter()
+
+while True:
+    start_ts = time.time()
+    state_v = torch.tensor(np.array([state], copy=False))
+    q_vals = net(state_v).data.numpy()[0]
+    action = np.argmax(q_vals)
+    c[action] += 1
+    state, reward, done, _ = env.step(action)
+    total_reward += reward
+    if RENDER:
+        env.render()
+        delta = 1/DESIRED_FPS - (time.time() - start_ts)
+        if delta > 0:
+            time.sleep(delta)
+    if done:
+        break
+
+print("Total reward: %.2f" % total_reward)
+print("Action counts:", c)
+env.close()
